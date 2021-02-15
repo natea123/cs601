60ba026cdf111cce96777676df8c59d735c72ac3
Nate <andrewsnate94@gmail.com>
2021-02-15 12:29:27 -0500

    add practice lessons

---
+++ week3/arrows.js
@@ -0,0 +1,15 @@
+
+
+
+//function addOne(value) { return (1 + value); }
+const addOne = x => x + 1;
+
+//function returnPair(evenVal) { return { evenVal: evenVal, oddVal: evenVal+1};}
+const returnPair = x => ({ evenVal: x, oddVal: (x+1) });
+
+//function sum(a,b) { return (a+b); };
+const sum = (a, b) => a + b;
+
+//function double(val) { return (val * val); }
+const double = x => x * x;
+
